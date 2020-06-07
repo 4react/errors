0dc6769303d3d4f18aa5c03f9f4991f8657a655b
zanomate <zanomate@gmail.com>
2020-06-07 09:48:19 +0200

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,48 +1,28 @@
-import commonjs from 'rollup-plugin-commonjs'
-import postcss from 'rollup-plugin-postcss'
-import { eslint } from 'rollup-plugin-eslint'
-import typescript from 'rollup-plugin-typescript2'
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "rollup-plugin-commonjs";
 
-import pkg from './package.json'
-
-const libraryName = 'errors'
-const globalLibs = Object.keys(pkg.dependencies)
-const externalLibs = Object.keys(pkg.peerDependencies)
+const pkg = require('./package.json')
 
 export default {
   input: 'src/index.ts',
-  output: [{
-    file: pkg.main,
-    format: 'umd',
-    globals: globalLibs,
-    name: libraryName
-  }, {
-    file: pkg.module,
-    format: 'es',
-    globals: globalLibs,
-    name: libraryName
-  }],
-  plugins: [
-    postcss({
-      modules: true
-    }),
-    eslint({
-      include: './src/**/*.tsx?'
-    }),
-    typescript({
-      clean: true,
-      typescript: require('typescript'),
-      verbosity: 0,
-      useTsconfigDeclarationDir: true,
-      tsconfigOverride: {
-        compilerOptions: {
-          module: "es2015"
-        }
-      }
-    }),
-    commonjs({
-      include: 'node_modules/**'
-    })
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true
+    }
   ],
-  external: externalLibs
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true })
+  ]
 }
